0b2929803f6b6deb895759bfbe08aafbf2a00334
David Sahakyan <davitsahakyan373@gmail.com>
2023-09-16 18:08:56 +0400

    First commit

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import './App.css'
+import RegisterPage from "../components/RegisterPage.js/RegisterPage";
+
+
+class App extends Component {
+    render() {
+
+        return (
+            <div>
+                <RegisterPage />
+            </div>)
+    }
+}
+
+export default App
